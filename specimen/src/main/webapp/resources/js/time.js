9087d748e0de0add34cd4aa1e9e625ed16eb1453
haram <ahr1025@naver.com>
2020-08-27 15:33:49 +0900

    update jsp

--- specimen/src/main/webapp/resources/js/time.js
+++ specimen/src/main/webapp/resources/js/time.js
@@ -1,19 +1,5 @@
-  <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<html>
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>Home</title>
-	<script type="text/javascript" src="/WEB/js/time.js"></script>
-</head>
-
-<!-- --> 
-
- 
- <!-- 
- <script type="text/javascript">
- function printClock(place){
-	 	var clock = document.getElementById(place);            // 출력할 장소 선택
+function printClock(){
+	 var clock = document.getElementById("clock");            // 출력할 장소 선택
 	    var currentDate = new Date();                                     // 현재시간
 	    var calendar = currentDate.getFullYear() + "-" + (currentDate.getMonth()+1) + "-" + currentDate.getDate() // 현재 날짜
 	    var amPm = 'AM'; // 초기값 AM
@@ -26,6 +12,9 @@
 	    	currentHours = addZeros(currentHours - 12,2);
 	    }
 
+	    if(currentSeconds >= 50){// 50초 이상일 때 색을 변환해 준다.
+	       currentSeconds = '<span style="color:#de1951;">'+currentSeconds+'</span>'
+	    }
 	    clock.innerHTML = currentHours+":"+currentMinute+":"+currentSeconds +" <span style='font-size:50px;'>"+ amPm+"</span>"; //날짜를 출력해 줌
 	    
 	    setTimeout("printClock()",1000);         // 1초마다 printClock() 함수 호출
@@ -41,13 +30,3 @@ function addZeros(num, digit) { // 자릿수 맞춰주기
 	  }
 	  return zero + num;
 }
-</script>
--->
-
-<body>
-	<body onload="printClock(clock)">
-		<div style="border:1px solid #dedede; width:600px; height:250px; line-height:250px; color:#666;font-size:100px; text-align:center;" id="clock">
-		</div>
-	</body>
-</body>
-</html>
